1458afe53b6cd952af311c8d755b9948ba002dc0
thara <thara@linecorp.com>
2015-12-31 13:54:51 +0900

    gitlab設定追加

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ cd ~/.ssh/
 # github, bitbucket接続用ssh_key生成
 ssh-keygen -t rsa -b 4096 -C "github" -f github_id_rsa
 ssh-keygen -t rsa -b 4096 -C "bitbucket" -f bitbucket_id_rsa
+ssh-keygen -t rsa -b 4096 -C "gitlab" -f gitlab_id_rsa
 
 # ssh関連設定コピー
 ln -s ~/git/mac-settings/dot-file/ssh/config ~/.ssh/
