161042fce66a0847ae75c16478fca0fb794ea221
thara <thara@linecorp.com>
2015-12-31 22:06:13 +0900

    php70

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ ssh-keygen -t rsa -b 4096 -C "gitlab" -f gitlab_id_rsa
 # ssh関連設定コピー
 ln -s ~/git/mac-settings/dot-file/ssh/config ~/.ssh/
 
+# php用のリポジトリをbrewに追加
+brew update
+brew tap homebrew/dupes
+brew tap homebrew/versions
+brew tap homebrew/homebrew-php
+brew tap homebrew/php
+brew install php70
+brew install php70-intl
 
-
+# cakephp setup用
+brew install composer
 
